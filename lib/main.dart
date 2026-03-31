bb0d68077787fb4def2c368e0a0d32b171f86365
Lalu_Farki <farkilalu1@gmail.com>
2026-03-31 08:06:35 +0700

    menambahkan firebase anoynmus authication

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'routes/app_routes.dart';
@@ -6,11 +7,10 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+  // Melakukan login anonim di background
+  await FirebaseAuth.instance.signInAnonymously();
   runApp(const MyApp());
 }
 
